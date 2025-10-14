f805187914c5fdb2159eea26a4e4364c6faa75eb
Umer Saleem <umersaleem64200@hotmail.com>
2025-10-14 15:15:21 -0400

    Update 4

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -34,10 +34,10 @@ const skillCategories = [
     icon: "",
     skills: [
       { name: 'Tableau', icon: '/logos/tableau.png', fontSize: 18 },
-      { name: 'Power BI', icon: '/logos/powerbi.PNG', fontSize: 18 },
-      { name: 'Matplotlib', icon: '/logos/matplotlib.PNG', fontSize: 18 },
+      { name: 'Power BI', icon: '/logos/powerbi.png', fontSize: 18 },
+      { name: 'Matplotlib', icon: '/logos/matplotlib.png', fontSize: 18 },
       { name: 'Seaborn', icon: '/logos/seaborn.SVG', fontSize: 18 },
-      { name: 'Plotly', icon: '/logos/plotly.PNG', fontSize: 18 },
+      { name: 'Plotly', icon: '/logos/plotly.png', fontSize: 18 },
     ],
   },
   {
