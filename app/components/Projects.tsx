b09046931c89084b7234dbdfcde217628c129a22
Umer Saleem <umersaleem64200@hotmail.com>
2025-12-16 12:20:32 -0500

    init

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -59,11 +59,11 @@ export default function Projects() {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <div className="w-full h-[90px] overflow-hidden rounded-lg mb-4 shadow-md hover:shadow-lg transition-shadow">
+                    <div className="w-full h-96 overflow-hidden rounded-lg mb-4 shadow-md hover:shadow-lg transition-shadow">
                       <img
                         src={project.thumbnailUrl}
                         alt={`${project.title} Thumbnail`}
-                        className="w-full h-full object-contain"
+                        className="w-full h-full object-cover"
                       />
                     </div>
                   </a>
